d4e4a7a631ceb98048a9cf0f32c743dcbc884f5d
RedwanParvez100 <19202103100@cse.bubt.edu.bd>
2024-11-20 13:11:13 +0600

    cors update in backend

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,16 @@ app.get("/", (req, res) => {
 
 app.use(bodyParser.json());
 app.use(cors());
+// Allow specific origin
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://mediusware-task-2-ui.vercel.app"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
 app.use("/auth", AuthRouter);
 app.use("/products", ProductRouter);
 
